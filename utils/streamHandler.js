549ff943026d8214da241a2d0b285e69d2b66613
abhinavswami <abhinav.swami21@gmail.com>
2017-09-09 23:41:15 -0700

    Persist twitter stream into MondoDb

---
+++ utils/streamHandler.js
@@ -0,0 +1,34 @@
+var Tweet = require('../models/Tweet');
+
+module.exports = function (stream, io) {
+    // When tweets get sent our way ...
+    stream.on('data', function (data) {
+        // Construct a new tweet object
+        console.log(data);
+        var tweet = {
+            twid: data['id'],
+            active: false,
+            author: data['user']['name'],
+            avatar: data['user']['profile_image_url'],
+            body: data['text'],
+            date: data['created_at'],
+            screenname: data['user']['screen_name']
+        };
+
+        // Create a new model instance of our object
+        var tweetEntry = new Tweet(tweet);
+
+        // Save tweet to the database
+        tweetEntry.save(function(err){
+            if(!err){
+                io.emit('tweet', tweet);
+            }
+            else
+                console.log('Error occurred:'+err);
+        });
+    });
+
+    stream.on('error', function(error, code) {
+        console.log("My error: " + error + ": " + code);
+        });
+}
