ba15c357dda239dd8119c97321f2a5f8973f448b
abhinavswami <abhinav.swami21@gmail.com>
2017-09-12 09:49:49 -0700

    Remove console prints

--- utils/streamHandler.js
+++ utils/streamHandler.js
@@ -4,7 +4,6 @@ module.exports = function (stream, io) {
     // When tweets get sent our way ...
     stream.on('data', function (data) {
         // Construct a new tweet object
-        console.log(data);
         var tweet = {
             twid: data['id'],
             active: false,
