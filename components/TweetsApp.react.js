ffac098300971ebbdcbeca04bb027a6d0a994e8a
abhinavswami <abhinav.swami21@gmail.com>
2017-09-12 09:49:13 -0700

    Add stub code

---
+++ components/TweetsApp.react.js
@@ -0,0 +1,11 @@
+var React = require('react');
+module.exports = TweetsApp = React.createClass({
+    // Render the component
+    render: function () {
+        return (
+            <div className="tweets-app">
+               
+            </div>
+        )
+    }
+});
