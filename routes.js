816494ad1b4e3500b2b267058ac17742e69d659d
abhinavswami <abhinav.swami21@gmail.com>
2017-09-12 09:46:50 -0700

    Use ReactDOM/Server

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 var JSX = require('node-jsx').install(),
     React = require('react'),
-    TweetsApp = require('./components/TweetsApp.react'),
+    ReactDOMServer = require('react-dom/server'),
+    TweetsApp = React.createFactory(require('./components/TweetsApp.react')),
     Tweet = require('./models/Tweet');
 
 module.exports = {
@@ -8,12 +9,12 @@ module.exports = {
         // Call static model method to get tweets in the db
         Tweet.getTweets(0, 0, function (tweets, pages) {
             // Render React to a string passing in fetched tweets
-            var markup = React.renderComponentToString(
-                TweetApp({
+            var markup = ReactDOMServer.renderToStaticMarkup(
+                TweetsApp({
                     tweets: tweets
                 })
             );
-
+            console.log('Before render');
             res.render('home', {
                 markup: markup,    // Pass rendered react to markup
                 state: JSON.stringify(tweets)   // Pass current state to client side
