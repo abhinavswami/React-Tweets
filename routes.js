0061afe032176d8f8a565226774deadb9ea564d1
Abhinav Swami <abhinav.swami21@gmail.com>
2017-09-16 22:42:29 -0700

    Fix a typo

--- routes.js
+++ routes.js
@@ -9,7 +9,7 @@ module.exports = {
         // Call static model method to get tweets in the db
         Tweet.getTweets(0, 0, function (tweets, pages) {
             // Render React to a string passing in fetched tweets
-            var markup = ReactDOMServer.renderToStaticMarkup(
+            var markup = ReactDOMServer.renderToString(
                 TweetsApp({
                     tweets: tweets
                 })
@@ -23,7 +23,7 @@ module.exports = {
     },
 
     page: function (req, res) {
-        Tweet.getTweets(req.parms.page, req.params.skip, function (tweets) {
+        Tweet.getTweets(req.params.page, req.params.skip, function (tweets) {
             // Render as json
             res.send(tweets);
         });
